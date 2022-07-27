0fe692dc8e92dbc329fb7dfa17586c8100b4e777
Aoik <aoi.kuiyuyou@gmail.com>
2022-07-27 17:06:06 +0800

    Mock backends handle cookie "session_id".

--- mock/user/login/api.ts
+++ mock/user/login/api.ts
@@ -13,15 +13,23 @@ import { ApiPath } from '../../../src/config/api_config';
 
 
 // -----
+const COOKIE_KEY_V_SESSION_ID = 'session_id';
+
+
+// -----
 export default {
-  // ----- 3Z8K4 -----
+  // ----- 5D7A1 -----
   [`POST ${ApiPath.API_USER_LOGIN}`]: async (req: Request, res: Response) => {
     const username = req?.body?.biz?.username;
     const password = req?.body?.biz?.password;
     if (password === 'a' && username === 'a') {
+      //
+      res.cookie(COOKIE_KEY_V_SESSION_ID, '1', { maxAge: 3600000, httpOnly: true });
+
+      // ----- 3Z8K4 -----
       res.send({
         base: {
-          loc: '4H1D6',
+          loc: '@5D7A1-#3Z8K4',
           apiLoc: '5D7A1',
           code: ApiResCode.BIZ_SUCC,
           status: ApiResStatus.HTTP_200_BIZ_SUCC,
@@ -31,9 +39,10 @@ export default {
       });
     }
     else {
+      // ----- 6F3M9 -----
       res.status(ApiResStatus.HTTP_299_BIZ_FAIL).send({
         base: {
-          loc: '6F3M9',
+          loc: '@5D7A1-#6F3M9',
           apiLoc: '5D7A1',
           code: ApiResCode.BIZ_FAIL,
           status: ApiResStatus.HTTP_299_BIZ_FAIL,
@@ -44,11 +53,15 @@ export default {
     }
   },
 
-  // ----- 7J4Y6 -----
+  // ----- 8C7I9 -----
   [`POST ${ApiPath.API_USER_LOGOUT}`]: (_req: Request, res: Response) => {
+    //
+    res.clearCookie(COOKIE_KEY_V_SESSION_ID)
+
+    // ----- 9Z4T3 -----
     res.send({
       base: {
-        loc: '9Z4T3',
+        loc: '@8C7I9-#9Z4T3',
         apiLoc: '8C7I9',
         status: ApiResStatus.HTTP_200_BIZ_SUCC,
         code: ApiResCode.BIZ_SUCC,
@@ -58,19 +71,36 @@ export default {
     });
   },
 
-  // ----- 1Z8G9 -----
-  [`POST ${ApiPath.API_USER_GET_AUTH_INFO}`]: (_req: Request, res: Response) => {
-    res.send({
-      base: {
-        loc: '2C4X1',
-        apiLoc: '3O1E7',
-        status: ApiResStatus.HTTP_200_BIZ_SUCC,
-        code: ApiResCode.BIZ_SUCC,
-        msg: ApiResMsg.SUCCESS,
-      },
-      biz: {
-        username: 'DemoUser',
-      },
-    });
+  // ----- 5D7A1 -----
+  [`POST ${ApiPath.API_USER_GET_AUTH_INFO}`]: (req: Request, res: Response) => {
+    //
+    if (!req.headers.cookie?.includes(COOKIE_KEY_V_SESSION_ID)) {
+      // ----- 8R3W1 -----
+      res.status(ApiResStatus.HTTP_299_BIZ_FAIL).send({
+        base: {
+          loc: '@5D7A1-#8R3W1',
+          apiLoc: '5D7A1',
+          code: ApiResCode.BIZ_FAIL,
+          status: ApiResStatus.HTTP_299_BIZ_FAIL,
+          msg: ApiResMsg.BIZ_FAIL,
+        },
+        biz: {},
+      });
+    }
+    else {
+      // ----- 2C4X1 -----
+      res.send({
+        base: {
+          loc: '@5D7A1-#2C4X1',
+          apiLoc: '5D7A1',
+          status: ApiResStatus.HTTP_200_BIZ_SUCC,
+          code: ApiResCode.BIZ_SUCC,
+          msg: ApiResMsg.BIZ_SUCC,
+        },
+        biz: {
+          username: 'DemoUser',
+        },
+      });
+    }
   },
 };
